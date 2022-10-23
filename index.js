8095609b2ce7e4972d2bbe1f424f1799809620d7
batuhanBASAK <batuhanbasak1997@gmail.com>
2022-10-23 22:29:39 +0300

    some html files have been updated

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const path = require("path");
 
 const port = 3000;
 
-
 // To use static files like img, css, and js files
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -15,23 +14,27 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-
 app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "/about.html"));
 });
-
 
 app.get("/go-room", (req, res) => {
   res.sendFile(path.join(__dirname, "/go-room.html"));
 });
 
-
 app.get("/create-room", (req, res) => {
+  // yeni room kur ve gerekli yerlere ekle.
+
+
+  // roomID yi html dosyası ile birlikte yolla.
   res.sendFile(path.join(__dirname, "/create-room.html"));
 });
 
 app.get("/room", (req, res) => {
   res.sendFile(path.join(__dirname, "/room.html"));
+
+
+  // roomID bir şeyler yapılmalı.
 });
 
 app.listen(port, () => {
