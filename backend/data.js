ec8203950e636a91d6eec9748a9f685480d27a01
borankurut <66724695+borankurut@users.noreply.github.com>
2022-11-11 20:32:34 +0300

    Add files via upload

--- backend/data.js
+++ backend/data.js
@@ -9,27 +9,39 @@ class User {
 }
 
 class Group {
-  constructor(size, users, code){
-    this.size = size; //change me
-    this.users = users; //members yap
+  constructor(admin, maxSize, code){  // todo: make the code randomly generated and unique.
+    this.admin = admin;
+    this.members = [admin];
     this.code =code;
-  }
-
-  addMember(user){
-    this.size++;
-    this.users.push(user);
-    user.groupCode = this.code;
+    this.maxSize = maxSize;
   }
   
-  discardMember(user){
-    this.users = this.users.filter((u) => u != user);
-    this.size--;
+  get size() {return this.members.length;}
+
+  addMember(user){
+    if(user.groupCode != '0')
+      throw new Error('User is already in the group');  // same or different.
+
+    if(this.size < this.maxSize){
+      this.members.push(user);
+      user.groupCode = this.code;
+    }
+    else 
+      throw new Error('Group is full.');
+  }
+  
+  discardMember(user){        // todo: handle when group size is 0
+    if(!this.members.includes(user))
+      throw new Error('User is not in the group');
+      
+    this.members = this.members.filter((member) => member != user);
+
+    if(user.id === this.admin.id && this.size > 0) // user is admin and there are members
+      this.admin = this.members[0];   // make the next user admin
+
     user.groupCode = '0';
   }
 }
-
-
-groups = [new Group(0, [], 'a1'), new Group(0, [], 'b2')];
 
 const users = [
   new User('1', 'erkutmail', 'erkut', 'erkut123', '0'),
@@ -38,9 +50,19 @@ const users = [
   new User('4', 'kadirmail', 'kadir', 'kadir123', '0'),
   new User('5', 'enesmail', 'enes', 'enes123', '0')
 ];
-groups[1].addMember(users[0]);
-groups[1].addMember(users[1]);
-console.log(groups[1].users);
 
+g1 = new Group(users[0], 2, 'codeG1');
+g2 = new Group(users[4], 3, 'codeG2');
+
+//console.log(g1, g2);
+
+g1.addMember(users[1]);
+//g1.addMember(users[2]);
+//g1.addMember(users[1]);   
+//console.log(g1, g2);
+
+g1.discardMember(users[0]);
+//g1.discardMember(users[3]);
+console.log(g1);
 
 module.exports = {users, User, Group};
