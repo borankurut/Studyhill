f6cdac69178a02d04f39ee98fc9289d6c7a140fc
batuhanBASAK <batuhanbasak1997@gmail.com>
2023-01-06 22:12:39 +0300

    userpage updated

--- client/src/components/UserProfilePage.js
+++ client/src/components/UserProfilePage.js
@@ -1,7 +1,7 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useLayoutEffect, useState, useRef } from "react";
-import moment from "moment";
+import moment, { localeData } from "moment";
 
 function UserProfilePage() {
   const [userName, setUserName] = useState("");
@@ -22,6 +22,8 @@ function UserProfilePage() {
 
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
+  const [newTask, setNewTask] = useState("");
+
   // Set each data for debugging.
   useLayoutEffect(() => {
     // Set current day using momentjs
@@ -34,7 +36,14 @@ function UserProfilePage() {
     // for the sake of rendering properly
     // because react useLayoutEffect is run after render.
     setBadges(["badge1", "badge2", "badge3"]);
-    setTasks(["task1", "task2", "task3", "task4"]);
+
+    // TODO
+    // Check whether tasks are stored in localstorage or not
+    // If stored get tasks
+    if (localStorage.getItem("tasks")) {
+      setTasks(JSON.parse(localStorage.getItem("tasks")));
+    }
+
     setWeeklyGoal(5);
     setweeklyHours({
       monday: 5,
@@ -80,7 +89,6 @@ function UserProfilePage() {
             else {
               // Set user informations here.
               setBadges([...res.data.badges]);
-              setTasks([...res.data.tasks]);
               setWeeklyGoal(res.data.weeklyGoal);
               setweeklyHours({ ...res.data.weeklyHours });
               setUserName(res.data.username);
@@ -134,6 +142,20 @@ function UserProfilePage() {
       setIsNavbarOpen(true);
     }
   };
+
+
+
+  // Add new task
+  const addNewTask = (e) => {
+    e.preventDefault();
+
+    if (newTask !== "") {
+      setTasks([...tasks, newTask]);
+      localStorage.setItem("tasks", tasks);
+    }
+  }
+
+
 
   return (
     <div className="container mx-auto">
@@ -209,24 +231,104 @@ function UserProfilePage() {
           </h2>
           <NavLink
             to="/study-page"
-            className="p-4 my-4 md:my-6 border border-white hover:border-slate-300 transition duration-150 ease-in font-bold text-sm md:text-base lg:text-lg xl:text-xl hover:text-slate-300 rounded-md"
+            className="flex gap-2 p-4 my-4 md:my-6 border border-white hover:border-slate-300 transition duration-150 ease-in font-bold text-sm md:text-base lg:text-lg xl:text-xl rounded-md hover:bg-white hover:text-navbar-dark"
           >
-            Start <span className="text-red-500">study</span>
+            Start Study
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+              />
+            </svg>
           </NavLink>
+
+          {/* Task Div */}
           <div className="border border-white rounded-lg px-6 py-4 flex flex-col items-center justify-center">
-            <button className="text-sm md:text-base lg:text-lg xl:text-xl font-extrabold mb-4">
+            <span className="text-sm md:text-base lg:text-lg xl:text-xl font-extrabold mb-4 flex gap-2 rounded-md">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 6.878V6a2.25 2.25 0 012.25-2.25h7.5A2.25 2.25 0 0118 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 004.5 9v.878m13.5-3A2.25 2.25 0 0119.5 9v.878m0 0a2.246 2.246 0 00-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0121 12v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6c0-.98.626-1.813 1.5-2.122"
+                />
+              </svg>
               Tasks
-            </button>
+            </span>
             <ul className="list-disc">
               {tasks.map((task, i) => (
                 <li
                   key={i}
-                  className="text-sm md:text-base lg:text-lg xl:text-xl font-light font-sans"
+                  className="flex gap-2 text-sm md:text-base lg:text-lg xl:text-xl font-light font-sans"
                 >
+                  {/* Task */}
                   {task}
+                  {/* Delete Button to delete current task */}
+                  <button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
                 </li>
               ))}
             </ul>
+
+            {/* Add task button */}
+            <form>
+              <input
+                type="text" 
+                className="text-black w-full my-4 p-1 outline-navbar-dark text-base rounded-sm"
+                required
+                onChange={(e) => setNewTask(e.target.value)}
+                placeholder="Please enter task here"
+              />
+              <button 
+                className="flex gap-2 font-sans font-semibold text-base text-white hover:text-slate-300"
+                onClick={addNewTask}  
+              >
+                
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                Add a new task
+              </button>
+            </form>
           </div>
         </aside>
 
@@ -237,7 +339,7 @@ function UserProfilePage() {
             {/* Weekly track div */}
             <div className="flex flex-col items-center justify-center py-2 gap-2">
               <h1 className="text-sm md:text-base lg:text-lg xl:text-xl font-semibold tracking-wider">
-                Weekly Track
+                Weekly Track {weeklyHours.date}
               </h1>
               <p className="text-xs md:text-sm lg:text-base xl:text-lg font-semibold tracking-wider">
                 Weekly Goal is {weeklyGoal}h
@@ -319,9 +421,22 @@ function UserProfilePage() {
                 <h1 className="font-extrabold font-sans text-sm md:text-base lg:text-lg xl:text-xl">
                   Badge Collection
                 </h1>
-                <button className="w-[37.5px] h-[37.5px] p-1 border-2 border-white rounded-md hover:border-slate-300 transition duration-150 ease-in">
-                  <img src="./img/icons8-trophy-cup-100.png" alt="Badge icon" />
-                </button>
+                <span className="p-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
+                    />
+                  </svg>
+                </span>
               </div>
               {/* Badge Collection */}
               <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
